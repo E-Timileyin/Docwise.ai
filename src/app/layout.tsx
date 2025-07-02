d69d599ebfb200e2dcb5ac72667129cd1bc19d8d
Eyiowuawi Timileyin <eyiowuawi.timileyin@gmail.com>
2025-07-02 04:14:39 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,84 @@
+import GithubIcon from "@/components/icons/github";
+import XIcon from "@/components/icons/x";
+import Logo from "@/components/ui/logo";
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+import Link from "next/link";
+import PlausibleProvider from "next-plausible";
+
+const font = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-plus-jakarta-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Smart PDFs | Summarize PDFs in seconds",
+  description:
+    "Upload a PDF to get a quick, clear, and shareable summary with AI for free!",
+  openGraph: {
+    images: "https://smartpdfs.vercel.app/og.jpg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full">
+      <head>
+        <PlausibleProvider domain="smartpdfs.ai" />
+      </head>
+      <body
+        className={`${font.variable} flex min-h-full flex-col bg-gray-100 font-[family-name:var(--font-plus-jakarta-sans)] text-gray-900 antialiased`}
+      >
+        <header className="py-6 text-center">
+          <Link href="/" className="inline-flex justify-center">
+            <Logo />
+          </Link>
+        </header>
+
+        <main className="grow overflow-hidden">{children}</main>
+        <Toaster />
+        <footer className="mx-auto mt-14 flex w-full max-w-7xl items-center justify-between px-4 py-6 md:mt-0">
+          <p className="text-xs text-gray-300 md:text-sm">
+            Powered by Llama 3.3 on{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 underline transition hover:text-gray-900"
+              href="https://togetherai.link/"
+            >
+              Together AI
+            </a>
+          </p>
+
+          <div className="flex items-center gap-2 md:gap-3">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center gap-1 rounded-lg border border-gray-250 bg-white px-2 py-1.5 text-xs text-gray-300 shadow transition hover:bg-white/75 md:rounded-xl md:px-4 md:text-sm"
+              href="https://github.com/nutlope/smartpdfs"
+            >
+              <GithubIcon className="size-4" />
+              GitHub
+            </a>
+            <a
+              href="https://x.com/nutlope"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center gap-1 rounded-lg border border-gray-250 bg-white px-2 py-1.5 text-xs text-gray-300 shadow transition hover:bg-white/75 md:rounded-xl md:px-4 md:text-sm"
+            >
+              <XIcon className="size-3" />
+              Twitter
+            </a>
+          </div>
+        </footer>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,223 @@
+"use client";
+
+import { useS3Upload } from "next-s3-upload";
+import { Button } from "@/components/ui/button";
+import {
+  Chunk,
+  chunkPdf,
+  generateImage,
+  generateQuickSummary,
+  summarizeStream,
+} from "@/lib/summarize";
+import { getDocument } from "pdfjs-dist/legacy/build/pdf.mjs";
+import { FormEvent, useState } from "react";
+import "pdfjs-dist/legacy/build/pdf.worker.mjs";
+import { MenuIcon, SquareArrowOutUpRight } from "lucide-react";
+import { sharePdf } from "@/app/actions";
+import ActionButton from "@/components/ui/action-button";
+import Link from "next/link";
+import { useToast } from "@/hooks/use-toast";
+import { HomeLandingDrop } from "@/components/HomeLandingDrop";
+import SummaryContent from "@/components/ui/summary-content";
+import TableOfContents from "@/components/ui/table-of-contents";
+
+export type StatusApp = "idle" | "parsing" | "generating";
+
+export default function Home() {
+  const [status, setStatus] = useState<StatusApp>("idle");
+  const [file, setFile] = useState<File>();
+  const [fileUrl, setFileUrl] = useState("");
+  const [chunks, setChunks] = useState<Chunk[]>([]);
+  const [activeChunkIndex, setActiveChunkIndex] = useState<
+    number | "quick-summary" | null
+  >(null);
+  const [quickSummary, setQuickSummary] = useState<{
+    title: string;
+    summary: string;
+  }>();
+  const [image, setImage] = useState<string>();
+  const [showMobileContents, setShowMobileContents] = useState(true);
+  const { uploadToS3 } = useS3Upload();
+
+  const { toast } = useToast();
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const language = new FormData(e.currentTarget).get("language");
+
+    if (!file || typeof language !== "string") return;
+
+    setStatus("parsing");
+
+    const uploadedPdfPromise = uploadToS3(file);
+
+    const arrayBuffer = await file.arrayBuffer();
+    const pdf = await getDocument({ data: arrayBuffer }).promise;
+    if (pdf.numPages > 500) {
+      toast({
+        variant: "destructive",
+        title: "PDF too large (500 pages max)",
+        description: "That PDF has too many pages. Please use a smaller PDF.",
+      });
+      setStatus("idle");
+      return;
+    }
+
+    const localChunks = await chunkPdf(pdf);
+    const totalText = localChunks.reduce(
+      (acc, chunk) => acc + chunk.text.length,
+      0,
+    );
+
+    if (totalText < 500) {
+      toast({
+        variant: "destructive",
+        title: "Unable to process PDF",
+        description:
+          "The PDF appears to be a scanned document or contains too little text to process. Please ensure the PDF contains searchable text.",
+      });
+      setFile(undefined);
+      setStatus("idle");
+      return;
+    }
+
+    setChunks(localChunks);
+    setStatus("generating");
+
+    const summarizedChunks: Chunk[] = [];
+
+    const writeStream = new WritableStream({
+      write(chunk) {
+        summarizedChunks.push(chunk);
+        setChunks((chunks) => {
+          return chunks.map((c) =>
+            c.text === chunk.text ? { ...c, ...chunk } : c,
+          );
+        });
+      },
+    });
+
+    const stream = await summarizeStream(localChunks, language);
+    const controller = new AbortController();
+    await stream.pipeTo(writeStream, { signal: controller.signal });
+
+    const quickSummary = await generateQuickSummary(summarizedChunks, language);
+    const imageUrl = await generateImage(quickSummary.summary);
+
+    setQuickSummary(quickSummary);
+    setImage(imageUrl);
+
+    const uploadedPdf = await uploadedPdfPromise;
+    setFileUrl(uploadedPdf.url);
+
+    setActiveChunkIndex((activeChunkIndex) =>
+      activeChunkIndex === null ? "quick-summary" : activeChunkIndex,
+    );
+
+    await sharePdf({
+      pdfName: file.name,
+      pdfUrl: uploadedPdf.url,
+      imageUrl: imageUrl,
+      sections: [
+        {
+          type: "quick-summary",
+          title: quickSummary.title,
+          summary: quickSummary.summary,
+          position: 0,
+        },
+        ...summarizedChunks.map((chunk, index) => ({
+          type: "summary",
+          title: chunk?.title ?? "",
+          summary: chunk?.summary ?? "",
+          position: index + 1,
+        })),
+      ],
+    });
+  }
+
+  return (
+    <div>
+      {status === "idle" || status === "parsing" ? (
+        <HomeLandingDrop
+          status={status}
+          file={file}
+          setFile={(file) => file && setFile(file)}
+          handleSubmit={handleSubmit}
+        />
+      ) : (
+        <div className="mt-6 px-4 md:mt-10">
+          <div className="mx-auto max-w-3xl">
+            <div className="flex items-center justify-between rounded-lg border border-gray-250 px-4 py-2 md:px-6 md:py-3">
+              <div className="inline-flex items-center gap-4">
+                <p className="md:text-lg">{file?.name}</p>
+              </div>
+
+              <div className="flex flex-row gap-2">
+                {fileUrl && (
+                  <Link href={fileUrl} target="_blank">
+                    <ActionButton>
+                      <SquareArrowOutUpRight size={14} />
+                      <span>Original PDF</span>
+                    </ActionButton>
+                  </Link>
+                )}
+              </div>
+            </div>
+
+            <div className="mt-8 rounded-lg bg-gray-200 px-4 py-2 shadow md:hidden">
+              <Button
+                onClick={() => setShowMobileContents(!showMobileContents)}
+                className="w-full text-gray-500 hover:bg-transparent"
+                variant="ghost"
+              >
+                <MenuIcon />
+                {showMobileContents ? "Hide" : "Show"} contents
+              </Button>
+
+              {showMobileContents && (
+                <div className="mt-4">
+                  <TableOfContents
+                    activeChunkIndex={activeChunkIndex}
+                    setActiveChunkIndex={setActiveChunkIndex}
+                    quickSummary={quickSummary}
+                    chunks={chunks}
+                  />
+                </div>
+              )}
+            </div>
+
+            <div className="mt-4 flex gap-4">
+              <div className="w-full grow rounded-lg bg-white p-5 text-gray-500 shadow">
+                {activeChunkIndex === "quick-summary" ? (
+                  <SummaryContent
+                    title={quickSummary?.title}
+                    summary={quickSummary?.summary}
+                    imageUrl={image}
+                  />
+                ) : activeChunkIndex !== null ? (
+                  <SummaryContent
+                    title={chunks[activeChunkIndex].title}
+                    summary={chunks[activeChunkIndex].summary}
+                  />
+                ) : (
+                  <div className="flex animate-pulse items-center justify-center py-4 text-lg md:py-8">
+                    Generating your Smart PDF&hellip;
+                  </div>
+                )}
+              </div>
+
+              <div className="hidden w-full max-w-60 shrink-0 md:flex">
+                <TableOfContents
+                  activeChunkIndex={activeChunkIndex}
+                  setActiveChunkIndex={setActiveChunkIndex}
+                  quickSummary={quickSummary}
+                  chunks={chunks}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
